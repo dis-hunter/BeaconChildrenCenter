21673c3f23f5feb95fc6c1df2b0d463b10236a7d
Sharon <sharon.kanyi@strathmore.edu>
2024-12-27 18:15:46 +0300

    made therapist page

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\TherapistController;
+
 //Doctor Form Routes
 Route::post('/doctors', [DoctorController::class, 'store']); // Handle form submission
 Route::view('/doctor_form', 'AddDoctor.doctor_form'); // Display the form
+
+//Therapist Routes
+Route::get('/therapist', [TherapistController::class, 'index'])->name('therapist.index');
+Route::post('/therapist/save', [TherapistController::class, 'saveTherapyNeeds'])->name('therapist.save');
+Route::get('/therapist/progress', [TherapistController::class, 'getProgress'])->name('therapist.progress');
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -25,4 +33,3 @@ Route::get('/doctor/{registrationNumber}', [DoctorsController::class, 'show'])->
 Route::get('/doctorDashboard', function () {
     return view('doctorDash');
 });
-
