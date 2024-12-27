22c1fee82b1d06c9f64e1119d6daf51800344058
Kristein-Gichuhi <kristein.mwaura@strathmore.edu>
2024-12-27 14:07:08 +0300

    backup

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function(){
-    return view('header');
+    return view('login');
 });
 
 Route::post('/example', [ExampleController::class,'store'])->name('example.store');
