8de1f75172990d87766f32526f0aef0ba85ef542
Emmanuel Oringe <emmanueloringe@gmail.com>
2025-01-04 12:21:09 +0300

    Updated behaviour assesment

--- app/Http/Controllers/GeneralExamController.php
+++ app/Http/Controllers/GeneralExamController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Log;
+
 
 class GeneralExamController extends Controller
 {
