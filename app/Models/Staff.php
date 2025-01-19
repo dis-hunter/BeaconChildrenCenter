6582f7896e56aa01c95a368d4d533544dc6a1e1a
Leriari <tobikoleriari@gmail.com>
2025-01-19 16:50:54 +0300

    visit handle once again

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -8,26 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Staff extends Model
 {
     use HasFactory;
-    protected $table='staff';
 
-<<<<<<< HEAD
-    public function specialization()
-=======
-    protected $fillable = ['fullname', 'telephone', 'email', 'email_verified', 'password', 'staff_no', 'remember_token', 'role_id', 'specialization_id', 'gender_id'];
+    protected $table = 'staff';
 
-<<<<<<< HEAD
+    protected $fillable = [
+        'fullname', 
+        'telephone', 
+        'email', 
+        'email_verified', 
+        'password', 
+        'staff_no', 
+        'remember_token', 
+        'role_id', 
+        'specialization_id', 
+        'gender_id'
+    ];
+
+    /**
+     * Define the relationship with DoctorSpecialization.
+     */
     public function doctors()
->>>>>>> 4ffaa8539790faf3134ff89a602e71fb7eeac372
     {
         return $this->belongsTo(DoctorSpecialization::class, 'specialization_id');
     }
-<<<<<<< HEAD
-
-    protected $fillable = [
-        'fullname', 'telephone', 'email', 'email_verified_at', 'password', 'staff_no', 'remember_token', 'gender_id', 'role_id', 'specialization_id'
-    ];
-=======
->>>>>>> 4ffaa8539790faf3134ff89a602e71fb7eeac372
-=======
->>>>>>> d72161ed07d123d1b0a77e9b4afff990f86026b0
 }
--- routes/web.php
+++ routes/web.php
@@ -69,8 +69,8 @@ Route::get('/children/create', [ChildrenController::class, 'create'])->name('chi
 Route::post('/children/store', [ChildrenController::class, 'store'])->name('children.store');
 
 // Doctor Routes
-// Route::get('/doctorslist', [DoctorController::class, 'index'])->name('doctors');
-// Route::view('/doctor_form', 'AddDoctor.doctor_form')->name('doctor.form'); 
+Route::get('/doctorslist', [DoctorController::class, 'index'])->name('doctors');
+Route::view('/doctor_form', 'AddDoctor.doctor_form')->name('doctor.form'); 
 Route::get('/doctors/specialization-search', [VisitController::class, 'showSpecializations'])->name('doctors.specializationSearch');
 Route::get('/specializations', [VisitController::class, 'getSpecializations']);
 Route::get('/doctors', [VisitController::class, 'getDoctorsBySpecialization']);
