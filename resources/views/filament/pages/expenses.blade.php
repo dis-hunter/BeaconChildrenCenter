5b2d4b8d17f325cbd52f7e4a4b10cac223210a8c
Sharon <sharon.kanyi@strathmore.edu>
2025-01-27 19:12:08 +0300

    styling form

--- resources/views/filament/pages/expenses.blade.php
+++ resources/views/filament/pages/expenses.blade.php
@@ -236,8 +236,7 @@
             }
         }
 
-        let descriptions = [];
-        /
+        let descriptions = []; // Your existing descriptions array stays the same
         switch (selectedCategory) {
             case "1":
                 descriptions = ["Rent", "Service Charge", "Other"];
@@ -262,7 +261,7 @@
                 ];
                 break;
             case "5":
-                descriptions = ["Software development", "Hardware purchase", "Techincal support",
+                descriptions = ["Software development", "Hardware purchase", "Technical support",
                     "Database hosting subscription", "Internet Service Providers", "Other"
                 ];
                 break;
@@ -272,7 +271,7 @@
                 ];
                 break;
             case "7":
-                descriptions = ["Online marketting", "Webinars", "Meeting costs", "Banners", "Marketting tools",
+                descriptions = ["Online marketing", "Webinars", "Meeting costs", "Banners", "Marketing tools",
                     "Branding", "Other"
                 ];
                 break;
