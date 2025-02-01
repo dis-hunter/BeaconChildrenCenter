e0a94eb73c527c41a1c2936bf31a0e5cf400188d
Oringe <emmanueloringe@gmail.com>
2025-02-01 09:27:23 +0300

    used Faker Library to add new child records to test Search engines

--- database/seeders/ChildSeeder.php
+++ database/seeders/ChildSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB; // Import the DB facade
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class ChildSeeder extends Seeder
 {
@@ -15,21 +15,24 @@ class ChildSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('children')->insert([
-            [
-                'fullname' => json_encode(['first_name' => 'John', 'middle_name' => 'Doe', 'last_name' => 'Smith']),
-                'dob' => '2018-05-10', 
-                'birth_cert' => 'BC/123456', 
-                'gender_id' => 1, 
-                'registration_number' => 'REG-001', 
-            ],
-            [
-                'fullname' => json_encode(['first_name' => 'Alice', 'middle_name' => 'Wilson', 'last_name' => 'Johnson']),
-                'dob' => '2020-01-22',
-                'birth_cert' => 'BC/789012',
-                'gender_id' => 2, 
-                'registration_number' => 'REG-002',
-            ],
-        ]);//
+        $faker = Faker::create(); // Create an instance of Faker
+
+        $children = [];
+
+        for ($i = 1; $i <= 100; $i++) {
+            $children[] = [
+                'fullname' => json_encode([
+                    'first_name' => $faker->firstName,
+                    'middle_name' => $faker->lastName, // Using lastName as middle name
+                    'last_name' => $faker->lastName
+                ]),
+                'dob' => $faker->date($format = 'Y-m-d', $max = 'now'), // Random date of birth
+                'birth_cert' => 'BC/' . $faker->numberBetween(100000, 999999), // Random birth certificate number
+                'gender_id' => $faker->numberBetween(1, 2), // Random gender (1 or 2)
+                'registration_number' => 'REG-' . $faker->unique()->numberBetween(1000, 9999), // Unique registration number
+            ];
+        }
+
+        DB::table('children')->insert($children); // Insert all 100 records at once
     }
 }
