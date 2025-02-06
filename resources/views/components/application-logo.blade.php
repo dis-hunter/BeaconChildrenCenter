a2c60b883c970df6f4951f86d9d0463f9e6307c3
victorgpt0 <175977320+victorgpt0@users.noreply.github.com>
2025-02-06 20:41:38 +0300

    patients bug fix

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,4 @@
-
-  <img src="{{ asset('images/logo-transparent.png') }}"
+<img src="{{ asset('images/logo-transparent.png') }}"
   style="width: 100px;"
        alt="{{ config('app.name') }} Logo" 
        class="logo-image"
