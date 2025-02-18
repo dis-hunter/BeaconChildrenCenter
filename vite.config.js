2092b545721d848c57b4b298752a3bef81241347
victorgpt0 <175977320+victorgpt0@users.noreply.github.com>
2025-02-18 19:31:59 +0300

    fix jetstream user profile

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
                 ],
             refresh: [
                 ...refreshPaths,
-                'app/Http/Livewire/**',
+                'app/Livewire/**',
             ],
         }),
     ],
