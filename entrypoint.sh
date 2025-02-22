ab51c2f224ca09a45837c2fd34899fc2472d7af4
victorgpt0 <175977320+victorgpt0@users.noreply.github.com>
2025-02-22 16:01:37 +0300

    start search locally

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,27 +4,21 @@ set -e
 # Start Redis Server
 service redis-server start
 
-
 # Start supervisor in the background
 /usr/bin/supervisord -c /etc/supervisor/conf.d/laravel-worker.conf &
 
 sleep 3
 
-#buld assets
-npm run build
-
 # Clear and cache config
 php artisan optimize
 
-until curl -s -f "${MEILISEARCH_HOST}/health" > /dev/null; do
-    echo "Waiting for Meilisearch to be ready..."
-    sleep 2
-done
+meilisearch --master-key=${MEILISEARCH_KEY} &
+
+sleep 3
 
 #Index the Models for the search engine
 php artisan scout:import "\App\Models\Parents"
 php artisan scout:import "\App\Models\children"
-
 
 php artisan queue:restart
 
