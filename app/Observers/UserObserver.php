412fa9fb7df124847628bd6e9e6c671631f81e94
victorgpt0 <175977320+victorgpt0@users.noreply.github.com>
2025-02-26 22:40:36 +0300

    profile info updated

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -3,16 +3,17 @@
 namespace App\Observers;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Log;
 
 class UserObserver
 {
     public function updated(User $user)
     {
-        cache()->forget('user_', $user->id);
+        cache()->forget('user_.'.$user->id);
     }
 
     public function deleted(User $user)
     {
-        cache()->forget('user_', $user->id);
+        cache()->forget('user_.'.$user->id);
     }
 }
