1dfd030c08538a2d52f94632ba070f3fa7d45ed0
Oringe <emmanueloringe@gmail.com>
2025-02-26 08:33:54 +0300

    Optimization

--- app/Filament/Pages/EncounterSummary.php
+++ app/Filament/Pages/EncounterSummary.php
@@ -5,9 +5,6 @@ namespace App\Filament\Pages;
 use Filament\Pages\Page;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Form;
-use Filament\Tables;
-use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
 use App\Models\Visits;
 
 class EncounterSummary extends Page
@@ -22,23 +19,21 @@ class EncounterSummary extends Page
     public function form(Form $form): Form
     {
         return $form->schema([
-            DatePicker::make('startDate')
-                ->label('Start Date')
-                ->reactive()
-                ->afterStateUpdated(fn () => $this->fetchVisits()),
-
-            DatePicker::make('endDate')
-                ->label('End Date')
-                ->reactive()
-                ->afterStateUpdated(fn () => $this->fetchVisits()),
+            DatePicker::make('startDate')->label('Start Date')->reactive(),
+            DatePicker::make('endDate')->label('End Date')->reactive(),
         ]);
     }
 
     public function fetchVisits()
     {
         if ($this->startDate && $this->endDate) {
-            $this->visits = Visits::whereBetween('visit_date', [$this->startDate, $this->endDate])
-                ->with(['child', 'doctor', 'visitType'])
+            $this->visits = Visits::select(['id', 'visit_date', 'child_id', 'doctor_id', 'visit_type'])
+                ->whereBetween('visit_date', [$this->startDate, $this->endDate])
+                ->with([
+                    'child:id,fullname',
+                    'doctor:id,fullname',
+                    'visitType:id,visit_type'
+                ])
                 ->get();
         }
     }
