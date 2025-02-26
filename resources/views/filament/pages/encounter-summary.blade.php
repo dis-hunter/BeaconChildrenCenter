76f8eccf5fca8c8aef275e9f5a7bc1cf3fd200fe
Oringe <emmanueloringe@gmail.com>
2025-02-26 08:20:05 +0300

    lets see

--- resources/views/filament/pages/encounter-summary.blade.php
+++ resources/views/filament/pages/encounter-summary.blade.php
@@ -9,41 +9,31 @@
         </form>
 
         @if (!empty($visits))
-            <table class="min-w-full bg-white border rounded-md shadow-md">
-                <thead>
-                    <tr class="bg-gray-100 border-b">
-                        <th class="p-2">Visit Date</th>
-                        <th class="p-2">Child Name</th>
-                        <th class="p-2">Doctor Name</th>
-                        <th class="p-2">Visit Type</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($visits as $visit)
-                        <tr class="border-b">
-                            <td class="p-2">{{ $visit->visit_date }}</td>
-                            <td class="p-2">
-    @php
-        $childName = is_string($visit->child->fullname) 
-            ? json_decode($visit->child->fullname, true) 
-            : (array) $visit->child->fullname;
-    @endphp
-    {{ $childName['first_name'] ?? '' }} {{ $childName['middle_name'] ?? '' }} {{ $childName['last_name'] ?? '' }}
-</td>
+        <table class="min-w-full border rounded-md shadow-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100">
+    <thead>
+        <tr class="bg-gray-100 dark:bg-gray-900 border-b">
+            <th class="p-2">Visit Date</th>
+            <th class="p-2">Child Name</th>
+            <th class="p-2">Doctor Name</th>
+            <th class="p-2">Visit Type</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($visits as $visit)
+            <tr class="border-b bg-white dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600">
+                <td class="p-2">{{ $visit->visit_date }}</td>
+                <td class="p-2">
+                    {{ $childName['first_name'] ?? '' }} {{ $childName['middle_name'] ?? '' }} {{ $childName['last_name'] ?? '' }}
+                </td>
+                <td class="p-2">
+                    {{ $doctorName['first_name'] ?? '' }} {{ $doctorName['middle_name'] ?? '' }} {{ $doctorName['last_name'] ?? '' }}
+                </td>
+                <td class="p-2">{{ $visit->visitType->visit_type }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
 
-<td class="p-2">
-    @php
-        $doctorName = is_string($visit->doctor->fullname) 
-            ? json_decode($visit->doctor->fullname, true) 
-            : (array) $visit->doctor->fullname;
-    @endphp
-    {{ $doctorName['first_name'] ?? '' }} {{ $doctorName['middle_name'] ?? '' }} {{ $doctorName['last_name'] ?? '' }}
-</td>
-                            <td class="p-2">{{ $visit->visitType->visit_type}}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
         @else
             <p class="text-gray-500">No visits found for the selected date range.</p>
         @endif
