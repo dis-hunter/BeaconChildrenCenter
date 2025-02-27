8b7d62b1b62fdeb28d9b24adc2a333c2df1e2dde
Oringe <emmanueloringe@gmail.com>
2025-02-27 09:24:27 +0300

    changed the env for testing new database

--- database/seeders/visit_typeSeeder.php
+++ database/seeders/visit_typeSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -16,18 +15,18 @@ class visit_typeSeeder extends Seeder
     public function run()
     {
         DB::table('visit_type')->insert([
-            ['visit_type' => 'Paediatric Consultation'],
-            ['visit_type' => 'General Consultation'],
-            ['visit_type' => 'Developmental Assessment'],
-            ['visit_type' => 'Occupational Therapy'],
-            ['visit_type' => 'Physiotherapy'],
-            ['visit_type' => 'Speech Therapy'],
-            ['visit_type' => 'Psychotherapy'],
-            ['visit_type' => 'Nutritionist'],
-            ['visit_type' => 'Medical Reports'],
-            ['visit_type' => 'Therapy Reports'],
-            ['visit_type' => 'Review'],
-            ['visit_type' => 'Other'],
+            ['visit_type' => 'Paediatric Consultation', 'normal_price' => 3000, 'sponsored_price' => 2000],
+            ['visit_type' => 'General Consultation', 'normal_price' => 3500, 'sponsored_price' => 2500],
+            ['visit_type' => 'Developmental Assessment', 'normal_price' => 4000, 'sponsored_price' => 3000],
+            ['visit_type' => 'Occupational Therapy', 'normal_price' => 3800, 'sponsored_price' => 2800],
+            ['visit_type' => 'Physiotherapy', 'normal_price' => 3700, 'sponsored_price' => 2700],
+            ['visit_type' => 'Speech Therapy', 'normal_price' => 3600, 'sponsored_price' => 2600],
+            ['visit_type' => 'Psychotherapy', 'normal_price' => 3400, 'sponsored_price' => 2400],
+            ['visit_type' => 'Nutritionist', 'normal_price' => 3300, 'sponsored_price' => 2300],
+            ['visit_type' => 'Medical Reports', 'normal_price' => 3200, 'sponsored_price' => 2200],
+            ['visit_type' => 'Therapy Reports', 'normal_price' => 3100, 'sponsored_price' => 2100],
+            ['visit_type' => 'Review', 'normal_price' => 3050, 'sponsored_price' => 2050],
+            ['visit_type' => 'Other', 'normal_price' => 2900, 'sponsored_price' => 2000],
         ]);
     }
 }
